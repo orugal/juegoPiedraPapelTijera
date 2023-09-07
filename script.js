683fc274e69c0488234f3e5ce660c9b326be97a6
Carlos Hernández <Calichi15@hotmail.com>
2023-09-07 12:26:14 -0500

    Refactorización del código

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
-var opciones = ['piedra','papel','tijera'];
-var puntajeUsuario = 0;
-var puntajeMaquina = 0;
+var options = ['piedra','papel','tijera'];
+var victim = [2,0,1];
+var userScore = 0;
+var cpuScore = 0;
 
-function juega(opcion){
-    var aleatorio = Math.floor(Math.random() * opciones.length);
-    var opcionMaquina = opciones[aleatorio];
-    var texto = "";
-    texto = "Usuario juega con: "+opcion+", Maquina juega con: "+opcionMaquina;
+function juega(userChoiceIndex){
+    let cpuChoiceIndex = Math.floor(Math.random() * options.length);
+
+    let userChoice = options[userChoiceIndex];
+    let cpuChoice = options[cpuChoiceIndex];
+    
+    let texto = `Usuario juega con: ${userChoice}, Maquina juega con: ${cpuChoice}`;
+
     //validamos.
-    if(opcion=='piedra' && opcionMaquina== 'piedra'){
-        alert(texto+". Empate")
-    }
-    else if(opcion == 'piedra' && opcionMaquina == 'papel'){
-        puntajeMaquina++;
-        alert(texto+". Gana la maquina.")
-    }
-    else if(opcion == 'piedra' && opcionMaquina == 'tijera'){
-        puntajeUsuario++;
-        alert(texto+". Gana usuario");
-    }
-    else if(opcion == 'papel' && opcionMaquina == 'papel'){
-        alert(texto+". Empate")
-    }
-    else if(opcion == 'papel' && opcionMaquina == 'piedra'){
-        puntajeUsuario++;
-        alert(texto+". Gana usuario")
-    }
-    else if(opcion == 'papel' && opcionMaquina == 'tijera'){
-        puntajeMaquina++;
-        alert(texto+". Gana maquina")
-    }
-    else if(opcion == 'tijera' && opcionMaquina == 'tijera'){
-        alert(texto+". Empate")
-    }
-    else if(opcion == 'tijera' && opcionMaquina == 'piedra'){
-        puntajeMaquina++;
-        alert(texto+". Gana maquina")
-    }
-    else if(opcion == 'tijera' && opcionMaquina == 'papel'){
-        puntajeUsuario++;
-        alert(texto+". Gana usuario")
+
+    if(userChoiceIndex === cpuChoiceIndex) {
+        alert(texto + ". Empate");
+    } else if (victim[userChoiceIndex] === cpuChoiceIndex) {
+        alert(texto + ". Gana el usuario");
+        userScore++;
+    } else {
+        alert(texto + ". Gana la máquina");
+        cpuScore++;
     }
 
-    document.getElementById("pu").innerHTML = puntajeUsuario;
-    document.getElementById("pm").innerHTML = puntajeMaquina;
+    document.getElementById("pu").innerHTML = userScore;
+    document.getElementById("pm").innerHTML = cpuScore;
 }
